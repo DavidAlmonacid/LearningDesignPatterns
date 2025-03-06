034c9469d85d5aa39a6e982a89c84ce44c937cff
David Almonacid <david.almonacidf@gmail.com>
2025-03-06 12:41:04 -0500

    Refactor LoggerAdapter to implement third-party logging library and enhance method signatures

--- src/main/kotlin/dev/designpatterns/structural/adapter/logger/LoggerAdapter.kt
+++ src/main/kotlin/dev/designpatterns/structural/adapter/logger/LoggerAdapter.kt
@@ -1,7 +1,22 @@
 package dev.designpatterns.structural.adapter.logger
 
-import io.github.oshai.kotlinlogging.KotlinLogging
+/*
+! Patrón Adapter
+Permite que objetos con interfaces incompatibles trabajen juntos, también
+es muy util para utilizar librerías de terceros en nuestra aplicación, sin
+depender directamente de ellas.
 
+* Es útil cuando se quiere reutilizar una clase que no tiene la interfaz que
+* necesitamos, o cuando queremos crear una capa de abstracción para una
+* librería de terceros.
+
+https://refactoring.guru/es/design-patterns/adapter
+ */
+
+// Third party library interface
+import io.github.oshai.kotlinlogging.KLogger
+
+// Custom interface
 interface LoggerAdapter {
     fun writeLog(vararg args: Any)
     fun writeWarning(vararg args: Any)
@@ -9,8 +24,8 @@ interface LoggerAdapter {
     fun writeDebug(vararg args: Any)
 }
 
-class LoggerAdapterImpl(file: String) : LoggerAdapter {
-    private val logger = KotlinLogging.logger(file)
+// Adapting the third party library to use the custom interface
+class LoggerAdapterImpl(private val logger: KLogger) : LoggerAdapter {
 
     override fun writeLog(vararg args: Any) {
         logger.info { args.joinToString() }
@@ -27,4 +42,5 @@ class LoggerAdapterImpl(file: String) : LoggerAdapter {
     override fun writeDebug(vararg args: Any) {
         logger.debug { args.joinToString() }
     }
+
 }
