f87bce6c8183b44407270213f5d85dcda6234991
David Almonacid <david.almonacidf@gmail.com>
2025-03-19 10:26:04 -0500

    Add documentation for Bridge Pattern in MainCharacter.kt

--- src/main/kotlin/dev/designpatterns/structural/bridge/character/MainCharacter.kt
+++ src/main/kotlin/dev/designpatterns/structural/bridge/character/MainCharacter.kt
@@ -1,5 +1,18 @@
 package dev.designpatterns.structural.bridge.character
 
+/*
+ * ! Patrón Bridge
+ * Este patrón nos permite desacoplar una abstracción de su implementación,
+ * de tal forma que ambas puedan variar independientemente.
+ *
+ * * Es útil cuando se tienen múltiples implementaciones de una abstracción.
+ * * Se puede utilizar para separar la lógica de negocio de la lógica de
+ * * presentación.
+ * * Se puede utilizar para separar la lógica de la interfaz de usuario también.
+ *
+ * https://refactoring.guru/es/design-patterns/bridge
+ */
+
 fun main() {
     println("\n -- Bridge Pattern -- \n")
 
